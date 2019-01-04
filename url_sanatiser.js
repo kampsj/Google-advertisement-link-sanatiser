e80c2fba66162510588e60d00158cc5fec804ba4
Jesse <jkamps@gmail.com>
2019-01-04 20:19:26 +0100

    Added basic functionality

--- url_sanatiser.js
+++ url_sanatiser.js
@@ -1,1 +1,24 @@
-// sanatiser code
+
+// Catch mouseclicks
+document.addEventListener('mousedown', linkClick, true);
+// Catch touch'clicks'
+document.addEventListener('touchstart', linkClick, true);
+
+
+
+function linkClick(event) {
+    // The proper link is present in the href on the google homepage.
+    var target = event.target;
+
+    // Check if target has an href attribute, if not go one level up.
+    while (!target.href) {
+        target = target.parentElement;
+    }
+
+    // Make sure Google's script wil not run.
+    target.removeAttribute('onmousedown');
+    target.removeAttribute('ontouchstart');
+
+
+
+}
